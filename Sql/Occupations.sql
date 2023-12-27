a3e08678956c20284616d207b4d8664cb08287f9
Arturo <arturocd@outlook.com>
2023-12-26 20:44:39 -0600

    Added solutions

--- Sql/Occupations.sql
+++ Sql/Occupations.sql
@@ -14,3 +14,26 @@ FROM
     )
 AS TABLE_WITH_RANKS
 GROUP BY RNK;
+
+
+SELECT
+    Doctor,
+    Professor,
+    Singer,
+    Actor
+FROM
+    (
+        SELECT
+            ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS row_number,
+            Name,
+            Occupation
+        FROM
+            OCCUPATIONS
+    )
+PIVOT
+    (
+        MIN(Name)
+        FOR Occupation IN ('Doctor' AS Doctor, 'Professor' AS Professor, 'Singer' AS Singer, 'Actor' AS Actor)
+    )
+ORDER BY
+    row_number ASC;
